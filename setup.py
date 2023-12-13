591f56951aa9dab266f0957da061c7b8fc4e6d52
sudoxroot <mrmrksminecraft@gmail.com>
2023-12-13 17:07:49 +0100

    🚑 fix missind "," lead to not installing

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'beautifulsoup4',
-        'python-dotenv'
+        'python-dotenv',
         'Requests',
         'tqdm',
     ],
